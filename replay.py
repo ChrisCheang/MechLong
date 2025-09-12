02e4c56907ad93ae0bbca32338e951154a79ecee
ChrisCheang <128516153+ChrisCheang@users.noreply.github.com>
2025-09-12 23:37:18 +0800

    Update replay.py
    Added first version of ball trajectory estimation

--- replay.py
+++ replay.py
@@ -111,11 +111,12 @@ ball2 = None
 g1 = None
 gc = None
 gc2 = None
+gc3 = None
 vp_lock = threading.Lock()
 
 
 def setup_vpython_vis():
-    global tx, ty, ball, ball2, g1, gc, gc2, camera_1_location, camera_1_direction, camera_2_location
+    global tx, ty, ball, ball2, g1, gc, gc2, gc3, camera_1_location, camera_1_direction, camera_2_location
 
     scene.width = 768#640  
     scene.height = 576#480
@@ -123,28 +124,29 @@ def setup_vpython_vis():
     scene.background = vec(0.7,0.7,0.7)
 
     g1 = graph(xtitle='time(s)',ytitle='speed(m/s)',xmin=0,ymin=0,ymax=20,align='left')
-    gc = gcurve(color=color.orange)
-    gc2 = gcurve(color=color.blue)
+    gc = gcurve(color=color.orange) #filtered
+    gc2 = gcurve(color=color.blue) #unfiltered
 
     box(pos=vector(tx/2, -0.005, -ty/2), size=vector(tx, 0.01, ty), color=vec(0.7,0.7,1)) # table 2.74, 1.525, 0.05
     box(pos=vector(tx/2, 0.1525/2, -ty/2), size=vector(tx, 0.1525, 0.001), color=vec(1,1,1))
 
 
-    #initialize ball
+    #initialize ball (filtered)
     ball = sphere(pos=vector(0, 0, 0), radius=0.021, color=color.orange, make_trail=True, retain=100)
     ball.trail_color = color.orange
-    ball.trail_radius = 0.002
+    ball.trail_radius = 0.005
 
-    #initialize ball 2
+    #initialize ball 2 (unfiltered)
     ball2 = sphere(pos=vector(0, 0, 0), radius=0.021, color=color.orange, make_trail=True, retain=100)
     ball2.trail_color = color.blue
-    ball2.trail_radius = 0.002
+    ball2.trail_radius = 0.005
+
+    gc3 = curve(color=color.black) #ball trajectory estimation
 
 
 
 
-def update_vpython_vis(intersection_point, index):
-    global ball, gc, cam1_line, cam2_line, ball_data#, graph
+def update_vpython_vis(intersection_point, index, ball, curve, data, re=False):
 
     if intersection_point == (0, 0, 0):
         return  # Skip invalid points
@@ -155,38 +157,68 @@ def update_vpython_vis(intersection_point, index):
         ball.pos = vector(x,z,-y)
 
         #for i in range(len(ball_data)):
-        times = [dic['time'] for dic in ball_data]
-        speeds = [dic['speed'] for dic in ball_data]
-        gc.plot(times[index]/1000,speeds[index])
-        if ball_data[index]['time'] > 1000:
-            g1.xmin = ball_data[index]['time']/1000 - 5
+        times = [dic['time'] for dic in data]
+        if re:
+            speeds = [dic['speed'][3] for dic in data]
+        else:
+            speeds = [dic['speed'] for dic in data]
+        curve.plot(times[index]/1000,speeds[index])
+        if data[index]['time'] > 1000:
+            g1.xmin = data[index]['time']/1000 - 5
         
         #except Exception as e:
             #logger.error(f"Error updating VPython visualization: {e}")
 
-def update_vpython_vis_unfiltered(intersection_point, index):
-    global ball2, gc2, cam1_line, cam2_line, ball_data_unfiltered#, graph
+def update_vpython_traj_projection(p,v):
+    global gc3
 
-    if intersection_point == (0, 0, 0):
-        return  # Skip invalid points
+    ts = np.linspace(0,1,10)
+
+    # clear old curve points
+    gc3.clear()
+
+    # draw new points
+    traj = Bounce(p,v)
+    for t in range(len(ts)):
+        point = traj.traj(ts[t])
+        gc3.append(pos=vector(point[0],point[2],-point[1]))
+
+
+
+class Bounce:
+    def __init__(self,p,v):
+        self.p = p
+        self.v = v
+
+    def traj_single(self, t):
+        x = self.p[0]+t*self.v[0]
+        y = self.p[1]+t*self.v[1]
+        z = self.p[2]+t*self.v[2]-0.5*9.81*t**2
+        return [x,y,z]
+
+    def firstbouncet(self):
+        discrim = (self.v[2])**2+4*0.5*9.81*self.p[2]
+        if discrim < 0:
+            return 0
+        else:
+            numer = self.v[2]+sqrt(discrim)
+            denom = 2*0.5*9.81
+            return numer/denom
     
-    with vp_lock:
-        #try:
-        x, y, z = intersection_point
-        ball2.pos = vector(x,z,-y)
-
-        #for i in range(len(ball_data)):
-        times = [dic['time'] for dic in ball_data_unfiltered]
-        speeds = [dic['speed'] for dic in ball_data_unfiltered]
-        gc2.plot(times[index]/1000,speeds[index])
-        if ball_data_unfiltered[index]['time'] > 1000:
-            g1.xmin = ball_data_unfiltered[index]['time']/1000 - 5
+    def pfirstbounce(self):
+        return self.traj_single(self.firstbouncet())
+    
+    def traj_second(self,t):
+        eball = 0.949 # empirical estimation of ball coeff of restitution
+        Bounce2 = Bounce(self.pfirstbounce(),[self.v[0],self.v[1],eball**2 * abs(self.v[2]-9.81*self.firstbouncet())])
+        return Bounce2.traj_single(t)
+    
+    def traj(self,t):
+        if t < self.firstbouncet():
+            return self.traj_single(t)
+        else:
+            return self.traj_second(t-self.firstbouncet())
         
-        #except Exception as e:
-            #logger.error(f"Error updating VPython visualization: {e}")
-
-
-
 
 
 # Read files. note: check if both files are the same length
@@ -198,7 +230,13 @@ with open('data_unfiltered_2025-09-07 16-11-25.json1', 'r') as file:
 
 # new dataset to test other filters
 no_points = 4
-ball_data_re = ball_data_unfiltered[:(no_points+1)] #initialise as first five unfiltered datapoints, in main would be five unknown ones.
+#ball_data_re = ball_data_unfiltered[:(no_points+1)] #initialise as first five unfiltered datapoints, in main would be five unknown ones.
+
+ball_data_re = [{'intersection': [0,0,0], 'time': 0, 'speed': [0,0,0,0]},  #note that the fourth number of speed is the magnitude for more convenience
+                {'intersection': [0,0,0], 'time': 10, 'speed': [0,0,0,0]},
+                {'intersection': [0,0,0], 'time': 20, 'speed': [0,0,0,0]},
+                {'intersection': [0,0,0], 'time': 30, 'speed': [0,0,0,0]},
+                {'intersection': [0,0,0], 'time': 40, 'speed': [0,0,0,0]}]
 
 def linear_regression_filter(xdata,ydata,x_extrapolate):
     x = np.array(xdata).reshape((-1,1))
@@ -218,26 +256,34 @@ for i in range(no_points+1,len(ball_data_unfiltered)-1):
     #z = linear_regression_filter(times, zs, ball_data_unfiltered[i+1]['time'])
 
     dt = ball_data_unfiltered[i]['time'] - ball_data_unfiltered[i-1]['time']
-    dt2 = ball_data_unfiltered[i]['time'] - ball_data_unfiltered[i-2]['time']
+    dt4 = ball_data_unfiltered[i]['time'] - ball_data_unfiltered[i-4]['time']
 
     #first order filter
-    x = filter(xs[-1],ball_data_re[-1]['intersection'][0],0.2,dt) #xs[-1]
+    x = filter(xs[-1],ball_data_re[-1]['intersection'][0],0.1,dt) #xs[-1]
     y = filter(ys[-1],ball_data_re[-1]['intersection'][1],0.02,dt) #ys[-1]
     z = filter(zs[-1],ball_data_re[-1]['intersection'][2],0.05,dt) #zs[-1]
 
-    speed = dist([x,y,z],ball_data_re[-2]['intersection'])/(dt2/1000)
-    speed = filter(speed,ball_data_re[-1]['speed'],0.05,dt)
+    speed = [0,0,0,0]
+
+    for coordinate in [0,1,2]:
+        speed[coordinate] = ([x,y,z][coordinate]-ball_data_re[-4]['intersection'][coordinate])/(dt4/1000)
+        speed[coordinate] = filter(speed[coordinate],ball_data_re[-1]['speed'][coordinate],0.05,dt)
+
+    speed[3] = dist([x,y,z],ball_data_re[-4]['intersection'])/(dt4/1000)
+    speed[3] = filter(speed[3],ball_data_re[-1]['speed'][3],0.05,dt)
 
     #x-jump filter (don't add new point if x jumps by more than a threshold difference (to remove large jumps from possible erronous detection), makes the filtered and unfiltered lists different in length so beware)
     x_jump = abs(x-ball_data_re[-1]['intersection'][0])
-    if x_jump < 0.25:
+    if x_jump < 1000:
         new_data = {'intersection': [x,y,z], 
                     'time': ball_data_unfiltered[i]['time'],
                     'speed': speed}   
-        ball_data_re.append(new_data) 
+        ball_data_re.append(new_data)
+
 
     print("calculating")
 print("done")
+
 
 
 start_time = int(time.time()*1000)
@@ -250,7 +296,7 @@ i = i_start # start index
 fast_forward = False
 
 pause = False
-now_time = ball_data[i_start]['time']
+now_time = ball_data_re[i_start]['time']
 play_time = now_time
 
 while i < i_end:
@@ -261,20 +307,24 @@ while i < i_end:
         pause = False
 
     if not pause:    
-        now_time = int(time.time()*1000)-start_time+ball_data[i_start]['time']
-        
+        now_time = int(time.time()*1000)-start_time+ball_data_re[i_start]['time']
+        #now_time += (int(time.time()*1000)-start_time)+now_time
+
         if not fast_forward:
-            if ball_data[i]['time'] < now_time:
-                update_vpython_vis(ball_data[i]['intersection'], index=i)
-                update_vpython_vis_unfiltered(ball_data_unfiltered[i]['intersection'], index=i) #times between the two sets match
+            if ball_data_re[i]['time'] < now_time:
+                update_vpython_vis(ball_data_re[i]['intersection'], index=i, data=ball_data_re, ball=ball, curve=gc, re=True)
+                #update_vpython_vis(ball_data_unfiltered[i]['intersection'], index=i, ball=ball2, curve=gc2, data=ball_data_unfiltered) #times between the two sets match
+                update_vpython_traj_projection(ball_data_re[i]['intersection'],ball_data_re[i]['speed'])
                 i += 1
         else:
-            update_vpython_vis(ball_data[i]['intersection'], index=i)
-            update_vpython_vis_unfiltered(ball_data_unfiltered[i]['intersection'], index=i) #times between the two sets match
+            update_vpython_vis(ball_data_re[i]['intersection'], index=i, data=ball_data_re, ball=ball, curve=gc, re=True)
+            #update_vpython_vis(ball_data_unfiltered[i]['intersection'], index=i, ball=ball2, curve=gc2, data=ball_data_unfiltered) #times between the two sets match
+            update_vpython_traj_projection(ball_data_re[i]['intersection'],ball_data_re[i]['speed'])
             i += 5
     else:
         start_time += int(time.time()*1000)-(now_time+start_time)
-    print(f"record time: {round(ball_data[i]['time']/1000,2)}, index = {i}, paused = {pause}, fastforward = {fast_forward}")
+    #print(now_time)
+    print(f"record time: {round(ball_data_re[i]['time']/1000,2)}, index = {i}, paused = {pause}, speed = ({round(ball_data_re[i]['speed'][0],2)},{round(ball_data_re[i]['speed'][1],2)},{round(ball_data_re[i]['speed'][2],2)})")
 
 
 
